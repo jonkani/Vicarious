c258cb9ef7f0ed9a179c7acb97b1a7c65f1b51a8
John Carney <jcarney000@gmail.com>
2016-09-10 18:38:36 -0700

    create server

--- app/components/SearchMap.jsx
+++ app/components/SearchMap.jsx
@@ -1,7 +1,11 @@
-import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
+import { GoogleMap, GoogleMapLoader, Marker } from 'react-google-maps';
 import React from 'react';
 
 const SearchMap = React.createClass({
+  handleTouchTap(event) {
+    console.log(event.latLng.lat(), event.latLng.lng());
+  },
+
   render() {
     return <section style={{ height: '500px' }}>
       <GoogleMapLoader
@@ -16,6 +20,7 @@ const SearchMap = React.createClass({
           <GoogleMap
             defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
             defaultZoom={3}
+            onClick={this.handleTouchTap}
           >
             <Marker
               position={{ lat: -25.363882, lng: 131.044922 }}
---
+++ server.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// CSRF protection
+app.use('/api', (req, res, next) => {
+  if (/json/.test(req.get('Accept'))) {
+    return next();
+  }
+
+  res.sendStatus(406);
+});
+
+app.use(bodyParser.json());
+
+app.use((_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  if (err.status || (err.output && err.output.statusCode)) {
+    return res
+      .status(err.status || err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+
+      // eslint-disable-next-line max-len
+      .send(err.errors ? err.errors[0].messages[0] : err.output.payload.message);
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port', port);
+});
