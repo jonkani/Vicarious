dc5c6cdd20842cf93f575e999ef9d22789d7bd79
John Carney <jcarney000@gmail.com>
2016-09-13 10:48:37 -0700

    Close #7 Limited the width of images returned from Flickr

--- routes/search.js
+++ routes/search.js
@@ -9,14 +9,14 @@ const router = express.Router();
 router.get('/search', (req, res, next) => {
   const { maxLat, minLat, maxLon, minLon } = req.query;
   const key = process.env.API_KEY;
-  const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${key}&bbox=${minLon}%2C${minLat}%2C${maxLon}%2C${maxLat}&group_id=44671723%40N00&extras=original_format%2Cgeo&format=json&nojsoncallback=1`;
+  const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${key}&bbox=${minLon}%2C${minLat}%2C${maxLon}%2C${maxLat}&group_id=44671723%40N00&extras=original_format%2Cgeo%2Curl_o&format=json&nojsoncallback=1`;
 
   axios.get(url)
     .then((response) => {
       let photos = response.data.photos.photo;
 
       photos = photos.filter((photo) => {
-        return photo.originalformat;
+        return photo.originalformat && Number.parseInt(photo.width_o) < 12000;
       });
       photos = photos.map((photo) => {
         const {
