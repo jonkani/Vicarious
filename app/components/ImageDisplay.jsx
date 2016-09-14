701895fe8d669728929a1b1f16ac7216ddab8ac9
John Carney <jcarney000@gmail.com>
2016-09-14 16:56:17 -0700

    Close #8 Create VR and back buttons for display page

--- app/components/ImageDisplay.jsx
+++ app/components/ImageDisplay.jsx
@@ -1,13 +1,18 @@
 import 'aframe';
+import { Entity, Scene } from 'aframe-react';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import NavBack from 'material-ui/svg-icons/navigation/arrow-back';
 import React from 'react';
 import { browserHistory } from 'react-router';
-import { Entity, Scene } from 'aframe-react';
-
 const ImageDisplay = React.createClass({
   componentWillMount() {
     if (!this.props.imageView.id) {
       browserHistory.push('/');
     }
+  },
+
+  handleButton() {
+    browserHistory.push('/');
   },
 
   render() {
@@ -18,21 +23,38 @@ const ImageDisplay = React.createClass({
       url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.originalsecret}_o.${photo.originalformat}`;
     }
 
-    return <Scene onLoaded={(event) => (console.log(event))}>
-      <a-assets>
-        <img id="displayimg" src={url} crossOrigin="anonymous" />
-      </a-assets>
-      <Entity
-        geometry={{
-          primitive: 'sphere',
-          radius: 5000,
-          segmentsWidth: 64,
-          segmentsHeight: 20
+    return <div style={{position: 'absolute', height: '100%', width: '100%'}}>
+      <Scene
+        onLoaded={(event) => (console.log(event))}
+        vr-mode-ui={{ enabled: 'true' }}
+      >
+        <a-assets>
+          <img crossOrigin="anonymous" id="displayimg" src={url} />
+        </a-assets>
+        <Entity
+          geometry={{
+            primitive: 'sphere',
+            radius: 5000,
+            segmentsWidth: 64,
+            segmentsHeight: 20
+          }}
+          material={{ src: '#displayimg' }}
+          scale={"1 1 -1"}
+        />
+      </Scene>
+      <FloatingActionButton
+        onTouchTap={this.handleButton}
+        style={{
+          zIndex: 999,
+          position: 'fixed',
+          bottom: '2%',
+          left: '2%'
         }}
-        material={{ src: '#displayimg' }}
-        scale={"1 1 -1"}
-      />
-    </Scene>;
+        zDepth={0}
+      >
+        <NavBack />
+      </FloatingActionButton>
+    </div>;
   }
 });
 
