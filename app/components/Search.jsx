46a4cef733f8f43a02770eab2c0d7922b1490b8e
John Carney <jcarney000@gmail.com>
2016-09-15 16:25:23 -0700

    add mid-monitor div

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -3,16 +3,18 @@ import SearchMap from './SearchMap';
 
 const Search = React.createClass({
   render() {
-    return <div className="monitor">
-      <div className="gradient">
-        <div className="logo"><span>V</span>ica<span>R</span>ious</div>
-        <SearchMap
-          imageList={this.props.imageList}
-          imageSearch={this.props.imageSearch}
-          searchLocation={this.props.searchLocation}
-          setCenter={this.props.setCenter}
-          setDisplay={this.props.setDisplay}
-        />
+    return <div className="midmonitor">
+      <div className="monitor">
+        <div className="gradient">
+          <div className="logo"><span>V</span>ica<span>R</span>ious</div>
+          <SearchMap
+            imageList={this.props.imageList}
+            imageSearch={this.props.imageSearch}
+            searchLocation={this.props.searchLocation}
+            setCenter={this.props.setCenter}
+            setDisplay={this.props.setDisplay}
+          />
+        </div>
       </div>
     </div>;
   }
